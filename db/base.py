5cead48b5177e7451aff97976d6540ae7210b5f9
SunnatDevPy <stalker.yoqubov@gmail.com>
2024-10-19 15:55:27 +0500

    commtted

--- db/base.py
+++ db/base.py
@@ -103,7 +103,7 @@ class AbstractClass:
     @classmethod
     async def get_from_referral_and_referred(cls, referrer_id, referred_user_id):
         query = select(cls).where(cls.referrer_id == referrer_id, cls.referred_user_id == referred_user_id)
-        return (await db.execute(query)).scalar().all()
+        return (await db.execute(query)).scalar()
 
     @classmethod
     async def get_from_referral_and_referred_all(cls, referrer_id, referred_user_id):
